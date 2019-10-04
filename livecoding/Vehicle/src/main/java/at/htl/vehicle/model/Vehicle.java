eca9aa977b6849064cdd7fd31ba0d65593488a20
htl-leonding <t.stuetz@htl-leonding.ac.at>
2019-10-04 14:45:21 +0200

    Entity Vehicle created

---
+++ livecoding/Vehicle/src/main/java/at/htl/vehicle/model/Vehicle.java
@@ -0,0 +1,40 @@
+package at.htl.vehicle.model;
+
+public class Vehicle {
+    private String brand;
+    private String type;
+
+    //region Constructors
+    public Vehicle() {
+    }
+
+    public Vehicle(String brand, String type) {
+        this.brand = brand;
+        this.type = type;
+    }
+    //endregion
+
+    //region getter and setter
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    //endregion
+
+
+    @Override
+    public String toString() {
+        return brand + ": " + type;
+    }
+}
