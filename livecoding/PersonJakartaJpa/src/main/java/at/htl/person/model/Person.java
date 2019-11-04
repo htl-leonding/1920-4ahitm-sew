94023b78933fa6c144eae34292a6ed005c0886be
htl-leonding <t.stuetz@htl-leonding.ac.at>
2019-11-04 10:47:59 +0100

    Entity Person created

---
+++ livecoding/PersonJakartaJpa/src/main/java/at/htl/person/model/Person.java
@@ -0,0 +1,50 @@
+package at.htl.person.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "MY_PERSON")
+public class Person {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String city;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d: %s (%s)",id,name,city);
+    }
+}
